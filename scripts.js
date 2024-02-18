17acaf9d9a2f275a5080b16614e085fb3afe7d85
DAV0KA <79053248901d@gmail.com>
2024-02-18 18:27:06 +0300

    modal close

--- scripts.js
+++ scripts.js
@@ -1,13 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
     const modalBtns = document.querySelectorAll('.modal-btn');
+
     modalBtns.forEach(function(btn) {
         btn.addEventListener('click', function() {
             const modalId = btn.getAttribute('data-modal');
             const modal = document.getElementById(modalId);
             modal.style.display = 'block';
+
             const closeBtn = modal.querySelector('.close-btn');
             closeBtn.addEventListener('click', function() {
                 modal.style.display = 'none';
+            });
+
+            // Добавляем обработчик события для клика вне модального окна
+            window.addEventListener('click', function(event) {
+                if (event.target === modal) {
+                    modal.style.display = 'none';
+                }
             });
         });
     });
